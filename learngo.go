32ae05365fe91db1b9544abc69aad4a3348b7913
Maria Cecelia Dime <Cel@Cels-MacBook-Pro.local>
2020-06-12 19:07:54 +0800

    Added Maps with interface

--- learngo.go
+++ learngo.go
@@ -1,29 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func backgroundTask() {
-	ticker := time.NewTicker(1 * time.Second)
-	for _ = range ticker.C {
-		fmt.Println("Tock")
-	}
+type Service interface {
+	SayHi()
+}
+
+type MyService struct{}
+
+func (s MyService) SayHi() {
+	fmt.Println("Hi")
+}
+
+type SecondService struct{}
+
+func (s SecondService) SayHi() {
+	fmt.Println("Hello From the 2nd Service")
 }
 
 func main() {
-	fmt.Println("Go Tickers Tutorial")
+	fmt.Println("Go Maps Tutorial")
+	// we can define a map of string uuids to
+	// the interface type 'Service'
+	interfaceMap := make(map[string]Service)
 
-	go backgroundTask()
+	// we can then populate our map with
+	// simple ids to particular services
+	interfaceMap["SERVICE-ID-1"] = MyService{}
+	interfaceMap["SERVICE-ID-2"] = SecondService{}
 
-	// This print statement will be executed before
-	// the first `tock` prints in the console
-	fmt.Println("The rest of my application can continue")
-	// here we use an empty select{} in order to keep
-	// our main function alive indefinitely as it would
-	// complete before our backgroundTask has a chance
-	// to execute if we didn't.
-	select {}
+	// Incoming HTTP Request wants service 2
+	// we can use the incoming uuid to lookup the required
+	// service and call it's SayHi() method
+	// interfaceMap["SERVICE-ID-2"].SayHi()
+
+	// Below we assign the "key" as the string service name of the map
+	// and the "service" as the struct value
+	// We could iterate over all the interfaces within our map
+	// and call every SayHi() method
+	for key, service := range interfaceMap {
+		fmt.Println(key)
+		service.SayHi()
+	}
 
 }
