59ac1252d9795081d39f2f945d0699b13d2c32e6
Maria Cecelia Dime <Cel@Cels-MacBook-Pro.local>
2020-06-20 15:13:36 +0800

    added new test file

--- learngo.go
+++ learngo.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"io/ioutil"
+	"os"
 )
 
-func CalculateValue(val chan int) {
-	value := rand.Intn(10)
-	fmt.Println("Calculated Random Value: {}", value)
-	// Passing the random generated value to the val channel
-	val <- value
-}
-
 func main() {
-	fmt.Println("Go Channel Tutorial")
 
-	values := make(chan int)
-	defer close(values)
-	// go routinge will generate a random value
-	// and will pass that values to "values" channel
-	go CalculateValue(values)
-	// value of the "values" channel will be pass a copy
-	// to the value variable to be printed out
-	value := <-values
-	fmt.Println(value)
+	mydata := []byte("All the data I wish to write to a file\n")
+
+	// the WriteFile method returns an error if unsuccessful
+	err := ioutil.WriteFile("myfile.data", mydata, 0777)
+	// handle this error
+	if err != nil {
+		// print it out
+		fmt.Println(err)
+	}
+
+	data, err := ioutil.ReadFile("myfile.data")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(string(data))
+
+	f, err := os.OpenFile("myfile.data", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	if _, err = f.WriteString("new data that wasn't there originally\n"); err != nil {
+		panic(err)
+	}
+
+	data, err = ioutil.ReadFile("myfile.data")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(string(data))
+
 }
