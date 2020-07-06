0c518959ca088c4fbb1e552aeb989bd6ff5b2077
Maria Cecelia Dime <Cel@Cels-MacBook-Pro.local>
2020-07-06 13:48:18 +0800

    added Channels with go routines

--- learngo.go
+++ learngo.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"sync"
 	"time"
 )
@@ -58,13 +60,17 @@ func createWorkerPool(noOfWorkers int) {
 	// line below will clouse the result channel of type Result struct
 	close(results)
 }
+
+// the func below takes the number of Jobs to be created as in put parameter
+// generates pseudo rando numbers with the max value set
+// creates Jobs struct using the random number and the for loop counter i as id
 func allocate(noOfJobs int) {
 	for i := 0; i < noOfJobs; i++ {
 		// enlarge the value of randomo to get a diffetent time execution output
-		randomno := rand.Intn(999)
+		randomno := rand.Intn(1000000)
 		// Job struct type is copied to job variable
 		job := Job{i, randomno}
-		// job variable is sent to the jobs channel of type Job struc
+		// job variable is sent to the jobs channel of type Job struct
 		jobs <- job
 	}
 	close(jobs)
@@ -76,9 +82,10 @@ func result(done chan bool) {
 	done <- true
 }
 func main() {
+
 	startTime := time.Now()
 	// enlarge the value of noOfJobs to get a different time execution output
-	noOfJobs := 10
+	noOfJobs := 99999
 	// allocate is called to add jobs to the jobs channel
 	go allocate(noOfJobs)
 	// done channel is created and passed to the result Goroutine so that it can start printing
@@ -87,11 +94,26 @@ func main() {
 	go result(done)
 	// a pool of  worker Goroutines are created by the call to createWorkerPool function
 	// enlarge the value of noOfWorkers to get a diffetrent time execution output
-	noOfWorkers := 10
+	noOfWorkers := 100
 	createWorkerPool(noOfWorkers)
 	// then main Goroutine waits on the done channel for all the results to tbe printed
 	<-done
 	endTime := time.Now()
-	diff := endTime.Sub(startTime)
-	fmt.Println("total time taken ", diff.Seconds(), "seconds")
+	diff := endTime.Sub(startTime).Seconds()
+
+	// Create a file and use os.OpenFile if file exist then append to it if not then create it
+	f, err := os.OpenFile("mydata.file", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	//	close file before main routine exits using defer
+	defer f.Close()
+	//	assign variable w as where to write buffer to file f
+	w := bufio.NewWriter(f)
+	//	write to buffer
+	fmt.Fprintf(w, "Total time taken %.8f seconds; Number of Jobs %d; Number of Workers %d\n", diff, noOfJobs, noOfWorkers)
+	// write whole chunk of buffer to file
+	w.Flush()
+	fmt.Println("total time taken ", diff, "seconds")
+
 }
--- test.go
+++ test.go
@@ -1144,6 +1144,40 @@ func CheckError(e error) {
 
 
 
+package main	// Simply Writting to file
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    // Create a file and use bufio.NewWriter.
+    f, _ := os.Create("C:\\programs\\file.txt")
+    w := bufio.NewWriter(f)
+
+    // Use Fprint to write things to the file.
+    // ... No trailing newline is inserted.
+    fmt.Fprint(w, "Hello")
+    fmt.Fprint(w, 123)
+    fmt.Fprint(w, "...")
+
+    // Use Fprintf to write formatted data to the file.
+    value1 := "cat"
+    value2 := 900
+    fmt.Fprintf(w, "%v %d...", value1, value2)
+
+    fmt.Fprintln(w, "DONE...")
+
+    // Done.
+    w.Flush()
+}
+
+
+
+
+
 
 package main	// Using Encryption to encrypt text or content
 
